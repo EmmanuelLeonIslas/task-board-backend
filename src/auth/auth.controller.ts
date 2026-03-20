c35e1ef85ed5b8ae7a5630ed666903b7ea01626a
Emmanuel León <cn-emmanuel@hotmail.com>
2026-03-20 16:01:57 -0600

    Created jwt.strategy

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,15 +5,15 @@ import { SignInDto } from './dto/signin.dto';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {}
 
-    @Post('/signup')
-    signUp(@Body() signUpDto: SignUpDto): Promise<{token: string}>{
-        return this.authService.signUp(signUpDto);
-    }
+  @Post('/signup')
+  signUp(@Body() signUpDto: SignUpDto): Promise<{ token: string }> {
+    return this.authService.signUp(signUpDto);
+  }
 
-    @Post('/signin')
-    signIn(@Body() signInDto: SignInDto): Promise<{token: string}>{
-        return this.authService.signIn(signInDto);
-    }
+  @Post('/signin')
+  signIn(@Body() signInDto: SignInDto): Promise<{ token: string }> {
+    return this.authService.signIn(signInDto);
+  }
 }
