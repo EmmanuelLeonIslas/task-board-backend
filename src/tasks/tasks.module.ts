bb93c30f3dc97c043fc0f615bbdc226881d6d0b4
Emmanuel León <cn-emmanuel@hotmail.com>
2026-03-21 00:20:43 -0600

    Added Guards to tasks

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { Task, TaskSchema } from './schemas/task.schema';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }])
+    MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }]),
+    AuthModule,
   ],
   controllers: [TasksController],
-  providers: [TasksService]
+  providers: [TasksService],
 })
 export class TasksModule {}
