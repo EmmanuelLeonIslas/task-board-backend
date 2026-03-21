bddb90a57c802d9693b96aac784306879e0357c5
Emmanuel León <cn-emmanuel@hotmail.com>
2026-03-21 00:20:43 -0600

    Added Guards to tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,37 +6,40 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
-    constructor(private readonly tasksService: TasksService) {}
+  constructor(private readonly tasksService: TasksService) {}
 
-    @Post()
-    create(@Body() createTaskDto: CreateTaskDto) {
-        return this.tasksService.create(createTaskDto);
-    }
+  @Post()
+  create(@Body() createTaskDto: CreateTaskDto) {
+    return this.tasksService.create(createTaskDto);
+  }
 
-    @Get()
-    findAll() {
-        return this.tasksService.findAll();
-    }
+  @Get()
+  findAll() {
+    return this.tasksService.findAll();
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.tasksService.findOne(id);
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.tasksService.findOne(id);
+  }
 
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() UpdateTaskDto: UpdateTaskDto) {
-        return this.tasksService.update(id, UpdateTaskDto);
-    }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() UpdateTaskDto: UpdateTaskDto) {
+    return this.tasksService.update(id, UpdateTaskDto);
+  }
 
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.tasksService.remove(id);
-    }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.tasksService.remove(id);
+  }
 }
