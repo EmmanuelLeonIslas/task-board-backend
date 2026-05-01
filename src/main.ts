0e311f7d964cc114d31ab764d5c2c83c070aaf7e
Emmanuel León <cn-emmanuel@hotmail.com>
2026-04-30 21:47:14 -0600

    Fixing port on main.ts

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,7 @@ async function bootstrap() {
   );
 
   // Railway asigna el puerto dinámicamente
-  const port = process.env.PORT || 3000;
+  const port = Number(process.env.PORT) || 3000;
   await app.listen(port, "0.0.0.0");
   console.log(`Application is running on port ${port}`);
 }
